1b5bd0ef572b56652cb0c8db92200a4631aa6d6d
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-15 20:47:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
-echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/Hacker-Jr-TG/Nancy-V3.5.git /Nancy-V3.5
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Hacker-Jr-TG/Nancy-V3.5.git /Nancy-V3.5
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Nancy-V3.5
+fi
 cd /Nancy-V3.5
-echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting Nancy-V3.5"
 python3 bot.py
