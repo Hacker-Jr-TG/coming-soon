1e6d40d01517f8266cd31874c4dbff8dadad6b81
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-20 01:03:24 +0530

    Create callbacks.py

---
+++ plugins/callbacks.py
@@ -0,0 +1,18 @@
+from pyrogram.types import CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup
+from pyrogram import Client
+from pyrogrambot.buttons import KGF_BUTTON
+import asyncio
+import random
+
+
+
+@Client.on_callback_query()
+async def callback(bot, msg: CallbackQuery):
+    elif msg.data == "downlod":
+        await msg.message.edit(
+            text="""<b>• Nᴀᴍᴇ : KGF
+• Yᴇᴀʀ : 2022
+• Sɪᴢᴇ : - 1400MB</b>""",
+            reply_markup=InlineKeyboardMarkup(KGF_BUTTON)
+        )
