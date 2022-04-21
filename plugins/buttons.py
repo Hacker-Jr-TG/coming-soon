8d1e6b9ee27fa7668588c8d074a0bd1104fe1f56
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-21 21:40:11 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -1,16 +1,18 @@
 from pyrogram.types import InlineKeyboardButton
 
 KGF_D_BUTTON = [[
-               InlineKeyboardButton("𝗖𝗛𝗔𝗣𝗧𝗘𝗥 𝟏", callback_data="downlod"),
-               InlineKeyboardButton("𝗖𝗛𝗔𝗣𝗧𝗘𝗥 𝟐", callback_data="smovies")
+               InlineKeyboardButton("CHAPTER 1", callback_data="downlod"),
+               InlineKeyboardButton("CHAPTER 2", callback_data="smovies")
                ]]
 
 KGF_BUTTON = [[
              InlineKeyboardButton("മലയാളം", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOAYAAnHRAVflSU0XBg9YoRYE"),
-             InlineKeyboardButton("TAMIL", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOQYAAnHRAVeazV9wvpi4SRYE"),
-             InlineKeyboardButton("HINDI", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPQYAAnHRAVergKA1x56J1hYE")
+             InlineKeyboardButton("TAMIL", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOQYAAnHRAVeazV9wvpi4SRYE")            
              ],[
+             InlineKeyboardButton("HINDI", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPQYAAnHRAVergKA1x56J1hYE"),
              InlineKeyboardButton("TELUGU", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOwYAAnHRAVc8R-uNd43pLBYE"),
-             InlineKeyboardButton("KANNADA", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPAYAAnHRAVd6dci1UtsI9BYE"),
-             InlineKeyboardButton("Cʟᴏꜱᴇ", callback_data="close_data")
+             InlineKeyboardButton("KANNADA", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPAYAAnHRAVd6dci1UtsI9BYE")
+             ],[
+             InlineKeyboardButton("⏪ Bᴀᴄᴋ", callback_data="KGF_D_BUTTON")
+             InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
              ]]
