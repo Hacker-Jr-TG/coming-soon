724e5879928d4fafd680f979787e2eda6e6632ec
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-21 19:54:20 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -275,7 +275,7 @@ async def delallconfirm(client, message):
         )
 
 @Client.on_message(filters.regex(['Kgf', 'kgf']) & filters.incoming)
-async def (client, message):
+async def Kgffilter(client, message):
     hegg = await message.reply_photo(
         photo=random.choice(KGF_PHOTOS),
         caption=f"""<b>🧿 Title : K.G.F: Chapter 2
