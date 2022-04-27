c314119a1f5f89ef3364cb02db1bbd3f87c2c3f3
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-04-27 14:55:34 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -1,23 +1,15 @@
 from pyrogram.types import InlineKeyboardButton
 
-KGF_D_BUTTON = [[
-               InlineKeyboardButton("KGF CHAPTER 1", callback_data="downlod"),
-               InlineKeyboardButton("KGF CHAPTER 2", callback_data="smovies")
-               ],[
-               InlineKeyboardButton("📙 1/1", "dupe"),
-               InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")            
-               ]]
-
-KGF_BUTTON = [[
-             InlineKeyboardButton("♻️ KGF : CHAPTER 1 ♻️", "dupe")            
+HEIST_BUTTON = [[
+             InlineKeyboardButton("🎭 MONEY HEIST SERIES 🎭", "dupe")            
              ],[
-             InlineKeyboardButton("മലയാളം", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOAYAAnHRAVflSU0XBg9YoRYE"),
-             InlineKeyboardButton("TAMIL", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOQYAAnHRAVeazV9wvpi4SRYE")            
+             InlineKeyboardButton("Season 1", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADYgUAAotaSVdYkQY0N8YL2BYE"),
+             InlineKeyboardButton("Season 2", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQAD9QMAAotaUVdwa8evBS6HLRYE")            
              ],[
-             InlineKeyboardButton("HINDI", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPQYAAnHRAVergKA1x56J1hYE"),
-             InlineKeyboardButton("TELUGU", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADOwYAAnHRAVc8R-uNd43pLBYE"),
-             InlineKeyboardButton("KANNADA", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADPAYAAnHRAVd6dci1UtsI9BYE")
+             InlineKeyboardButton("Season 3", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADigUAAr4wSVeWyFZcTuuM3xYE"),
+             InlineKeyboardButton("Season 4", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADiwUAAr4wSVcpwLLQSRfk6RYE"),
+             InlineKeyboardButton("Season 5", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADjAUAAr4wSVd5ZIiqNPd8WhYE")
              ],[
-             InlineKeyboardButton("⏪ Bᴀᴄᴋ", callback_data="kgfdownlod"),
+             InlineKeyboardButton("📗 1/1", "dupe"),
              InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
              ]]
