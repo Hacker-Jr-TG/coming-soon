502d561c0af03bb83c721005526fc288559aca5b
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-05-04 18:19:23 +0530

    Update photos.py

--- plugins/photos.py
+++ plugins/photos.py
@@ -1,6 +1,4 @@
 
-KGF_PHOTOS = [
-             "https://telegra.ph/file/1a39311dbf13ca400ee5a.jpg",
-             "https://telegra.ph/file/f83a205dcfd1cdc33baab.jpg",
-             "https://telegra.ph/file/8712112da8ba9f593e604.jpg",            
+CBI_PHOTOS = [
+             "https://telegra.ph/file/a1b02c96a0a45040d9a15.jpg"            
              ]
