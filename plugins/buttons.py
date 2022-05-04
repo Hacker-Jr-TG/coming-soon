8a6e74906c42a8d256b23f01d32ab486dafd21e5
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-05-04 18:26:05 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -1,15 +1,14 @@
 from pyrogram.types import InlineKeyboardButton
 
-HEIST_BUTTON = [[
-             InlineKeyboardButton("🎭 MONEY HEIST SERIES 🎭", "dupe")            
+CBI_BUTTON = [[
+             InlineKeyboardButton("🔮 CBI MOVIES COLLECTION 🔮", "dupe")            
              ],[
-             InlineKeyboardButton("Season 1", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADYgUAAotaSVdYkQY0N8YL2BYE"),
-             InlineKeyboardButton("Season 2", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQAD9QMAAotaUVdwa8evBS6HLRYE")            
+             InlineKeyboardButton("1. Oru CBI Dairy Kuripp (1988)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADYgUAAotaSVdYkQY0N8YL2BYE"),
+             InlineKeyboardButton("2. CBI Jaagratha (1989)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQAD9QMAAotaUVdwa8evBS6HLRYE"),            
+             InlineKeyboardButton("3. Sethurama Iyer CBI (2004)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADigUAAr4wSVeWyFZcTuuM3xYE"),
+             InlineKeyboardButton("4. Nerariyan CBI (2005)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADiwUAAr4wSVcpwLLQSRfk6RYE"),
+             InlineKeyboardButton("5. CBI 5: The Brain (2022)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADjAUAAr4wSVd5ZIiqNPd8WhYE")
              ],[
-             InlineKeyboardButton("Season 3", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADigUAAr4wSVeWyFZcTuuM3xYE"),
-             InlineKeyboardButton("Season 4", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADiwUAAr4wSVcpwLLQSRfk6RYE"),
-             InlineKeyboardButton("Season 5", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADjAUAAr4wSVd5ZIiqNPd8WhYE")
-             ],[
-             InlineKeyboardButton("📗 1/1", "dupe"),
+             InlineKeyboardButton("📙 1/1", "dupe"),
              InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
              ]]
