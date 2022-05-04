ff362facc1f57d0da83a4fed522b173b34ecc7ca
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2022-05-04 19:05:06 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -3,12 +3,16 @@ from pyrogram.types import InlineKeyboardButton
 CBI_BUTTON = [[
              InlineKeyboardButton("🔮 CBI MOVIES COLLECTION 🔮", "dupe")            
              ],[
-             InlineKeyboardButton("1. Oru CBI Dairy Kuripp (1988)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADYgUAAotaSVdYkQY0N8YL2BYE"),
-             InlineKeyboardButton("2. CBI Jaagratha (1989)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQAD9QMAAotaUVdwa8evBS6HLRYE"),            
-             InlineKeyboardButton("3. Sethurama Iyer CBI (2004)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADigUAAr4wSVeWyFZcTuuM3xYE"),
-             InlineKeyboardButton("4. Nerariyan CBI (2005)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADiwUAAr4wSVcpwLLQSRfk6RYE"),
+             InlineKeyboardButton("1. Oru CBI Dairy Kuripp (1988)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADYgUAAotaSVdYkQY0N8YL2BYE")
+             ],[
+             InlineKeyboardButton("2. CBI Jaagratha (1989)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQAD9QMAAotaUVdwa8evBS6HLRYE")
+             ],[            
+             InlineKeyboardButton("3. Sethurama Iyer CBI (2004)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADigUAAr4wSVeWyFZcTuuM3xYE")
+             ],[
+             InlineKeyboardButton("4. Nerariyan CBI (2005)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADiwUAAr4wSVcpwLLQSRfk6RYE")
+             ],[
              InlineKeyboardButton("5. CBI 5: The Brain (2022)", url="https://t.me/Oru_adaar_Robot?start=BATCH-BQADBQADjAUAAr4wSVd5ZIiqNPd8WhYE")
              ],[
-             InlineKeyboardButton("📙 1/1", "dupe"),
+             InlineKeyboardButton("📗 1/1", "dupe"),
              InlineKeyboardButton("🔐 Cʟᴏꜱᴇ", callback_data="close_data")
              ]]
